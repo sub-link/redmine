a50b39f03dfe0aedccfa07b3886d1c8fb20bca64
mkoentopf <mkoentopf@sub-link.net>
2019-07-05 11:12:11 +0200

    Add no permission check to receive mails
    Sometime mail were ignored

--- receive-mails.sh
+++ receive-mails.sh
@@ -1,7 +1,7 @@
 # @Author: mkoentopf
 # @Date:   2017-01-19 11:58:22
 # @Last Modified by:   mkoentopf
-# @Last Modified time: 2017-01-19 11:59:37
+# @Last Modified time: 2019-07-05 11:10:39
 
 # */5 * * * * root  bash -l /srv/www/redmine/receive-mails.sh
 
@@ -10,4 +10,4 @@ cd /srv/www/redmine
 rake -f /srv/www/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production \
 host=mail.sub-link.de username="inca-redmine@sub-link.de" password=qeGni0Fk \
 project=inca-mail-tickets allow_override=all tracker=Support status=Neu priority=Normal \
-unknown_user=accept
+unknown_user=accept no_permission_check=1
